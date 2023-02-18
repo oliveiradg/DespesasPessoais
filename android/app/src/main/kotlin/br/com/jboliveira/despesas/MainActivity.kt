a9cf7166846c6b81a170d6b4612c2391b9037e64
João Oliveira <j.oliveiradg@gmail.com>
2023-02-18 16:36:59 -0300

    [DIA 41] first commit, CRIANDO O PROJETO

---
+++ android/app/src/main/kotlin/br/com/jboliveira/despesas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.jboliveira.despesas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatelessWidget {
+
+  const Main({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Despesas pessoais'),),
+           body: Container(),
+       );
+  }
+}
