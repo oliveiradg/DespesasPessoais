a589f984e677eca56f1f0c5635cb251bfc234ede
João Oliveira <j.oliveiradg@gmail.com>
2023-02-18 17:09:56 -0300

    [DIA 41] feat: Combinando Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
 
-class Main extends StatelessWidget {
+void main() {
+  runApp(const MyApp());
+}
 
-  const Main({ Key? key }) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Despesas Pessoais',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatelessWidget {
+
+  const MyHomePage({ Key? key }) : super(key: key);
 
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Despesas pessoais'),),
-           body: Container(),
+           appBar: AppBar(title: const Text('Despesas Pessoais'),),
+           body: Column(
+                children: const [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        color: Colors.blue,
+                          elevation: 5,
+                          child:Text('Gráfico'),
+                      ),
+                    ),
+                    Card(
+                        elevation: 5,
+                        child: Text('Lista de Transações'),
+                    ),
+                ],
+           ),
        );
   }
 }
