3f8677df67b6d2cf1e6cc661f949d3cba238f420
João Oliveira <j.oliveiradg@gmail.com>
2023-02-22 23:38:26 -0300

    [DIA 45] feat: Criando Classe de Modelo Transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas/models/transacao.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,40 +14,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Despesas Pessoais',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home:  MyHomePage(),
     );
   }
 }
 
-
-
 class MyHomePage extends StatelessWidget {
+  final _transacao = [
+    Transacao(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transacao(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
-  const MyHomePage({ Key? key }) : super(key: key);
+   MyHomePage({Key? key}) : super(key: key);
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Despesas Pessoais'),),
-           body: Column(
-                children: const [
-                    SizedBox(
-                      width: double.infinity,
-                      child: Card(
-                        color: Colors.blue,
-                          elevation: 5,
-                          child:Text('Gráfico'), 
-                      ),
-                    ),
-                    Card(
-                        elevation: 5,
-                        child: Text('Lista de Transações'),
-                    ),
-                ],
-           ),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        children: const [
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
+    );
   }
 }
